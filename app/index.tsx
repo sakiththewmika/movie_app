8ab79ffa84e0ea4fd380dbaef3474f824d2c30b3
sakiththewmika <sakiththewmika@gmail.com>
2025-05-02 14:41:33 +0530

    Setup styling

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-bold text-dark-100">Welcome!!</Text>
+    </View>
+  );
+}

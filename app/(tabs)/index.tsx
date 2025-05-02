1b442c80bc6deada81796e2f9280d8c410ce8384
sakiththewmika <sakiththewmika@gmail.com>
2025-05-02 17:05:11 +0530

    Add background image and logo to the home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-bold text-dark-100">Welcome!!</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 bg-primary">
+            <Image source={images.bg} className="absolute w-full z-0" />
+            <ScrollView className="flex-1 px-5" 
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    minHeight: "100%",
+                    paddingBottom: 10
+                }}>
+                    <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+            </ScrollView>
+        </View>
+    );
 }

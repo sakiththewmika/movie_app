61e451edb6695fce6ad08c5f61b6726bc63ba25e
sakiththewmika <sakiththewmika@gmail.com>
2025-05-02 15:30:46 +0530

    Add navigation tabs and assets

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>Movie Details</Text>
+    </View>
+  )
+}
+
+export default MovieDetails
+
+const styles = StyleSheet.create({})
